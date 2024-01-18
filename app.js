d15da53be74ad524251432318bfb840f2f9d2c14
NerokArtworks <nerokprodscontact@gmail.com>
2024-01-18 18:15:04 +0100

    update

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const adviceId = document.querySelector('span');
 const quote = document.querySelector('h1');
 
 dice.addEventListener('click', async function (e) {
+    generateNewAdvice();
+});
+
+const generateNewAdvice = async () => {
     const { slip } = await fetch('https://api.adviceslip.com/advice')
                                     .then(response => response.json());
     const { id: slip_id, advice } = slip;
     adviceId.textContent = `#${slip_id}`;
     quote.textContent = advice;
-});
+}
+
+// Generate first advice on load
+generateNewAdvice();
